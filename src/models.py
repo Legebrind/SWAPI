9f21492848a9034885847235d31567109c377cfb
Legebrind <moyi88@gmail.com>
2022-09-05 16:39:17 +0000

    Added relationship

--- src/models.py
+++ src/models.py
@@ -2,23 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+PeopleFavorite = db.Table("PeopleFavorite",  
+    db.Column("userId",db.Integer, db.ForeignKey("user.id"),primary_key=True),
+    db.Column("peopleId",db.Integer, db.ForeignKey("people.id"),primary_key=True))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            "is_active" : self.is_active
-            # do not serialize the password, its a security breach
-        }
-
+PlanetFavorite = db.Table("PlanetFavorite",
+    db.Column("userId",db.Integer, db.ForeignKey("user.id"),primary_key=True),
+    db.Column("planetId",db.Integer, db.ForeignKey("planet.id"),primary_key=True))
+    
 class People(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
@@ -55,34 +46,48 @@ class Planet(db.Model):
             "terrain": self.terrain            
         }
 
-class PeopleFavorite(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, unique=True, nullable=False)
-    peopleId = db.Column(db.Integer, unique=False, nullable=False)
-    
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    people_favorite = db.relationship('People', secondary=PeopleFavorite, lazy='subquery',
+        backref="favorite people")
+    planet_favorite = db.relationship('Planet', secondary=PlanetFavorite, lazy='subquery',
+        backref="favorite planets")
 
     def __repr__(self):
-        return f'<PeopleFavorite id ={self.id}, userId ={self.userId}, peopleId ={self.peopleId}>'
+        return '<User %r>' % self.username
 
     def serialize(self):
         return {
             "id": self.id,
-            "userId": self.userId,
-            "peopleId": self.peopleId                       
+            "email": self.email,
+            "is_active" : self.is_active
+            # do not serialize the password, its a security breach
         }
 
-class PlanetFavorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, unique=True, nullable=False)
-    planetId = db.Column(db.Integer, unique=False, nullable=False)
+
     
 
-    def __repr__(self):
-        return f'<PeopleFavorite id ={self.id}, userId ={self.userId}, planetId ={self.planetId}>'
+"""  def __repr__(self):
+    return f'<PeopleFavorite id ={self.id}, userId ={self.userId}, peopleId ={self.peopleId}>'
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "userId": self.userId,
-            "planetId": self.planetId                       
-        }
+def serialize(self):
+    return {
+        "id": self.id,
+        "userId": self.userId,
+        "peopleId": self.peopleId                       
+    } """
+
+
+
+""" def __repr__(self):
+    return f'<PeopleFavorite id ={self.id}, userId ={self.userId}, planetId ={self.planetId}>'
+
+def serialize(self):
+    return {
+        "id": self.id,
+        "userId": self.userId,
+        "planetId": self.planetId                       
+    } """
